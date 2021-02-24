cd9a35f4c8b361306adf2aef1eb488423df81210
Rey Vega <A01114523@itesm.mx>
2021-02-23 20:18:26 -0700

    Solve reverse-word
    Signed-off-by: Rey Vega <A01114523@itesm.mx>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-    // Place your magic here
+int main()
+{
+    int i = 0;
+    char c;
+    char* word = malloc(100 * sizeof(char));
+
+    while ((c = getchar()) != EOF)
+    {
+        if (c == '\n')
+        {
+            for (int j = i; j >= 0; j--)
+            {
+                printf("%c", word[j]);
+            }
+            i = 0;
+            memset(word, 0, sizeof word);
+            printf("\n");
+        }
+        else
+        {
+            word[i++] = c;
+        }
+    }
+    free(word);
     return 0;
 }
