39f28583cc56b41e92c7c9a979fcc72c01688591
Rey Vega <A01114523@itesm.mx>
2021-03-16 16:54:40 -0700

    Solved Lab - Month Calculator
    Signed-off-by: Rey Vega <A01114523@itesm.mx>

--- labs/month-day/month_day.c
+++ labs/month-day/month_day.c
@@ -1,56 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static int plus = 0;
-
 static char daytab[2][12] = {
     {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
-    {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
-};
+    {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
 
 /* month_day function's prototype*/
 void month_day(int year, int yearday, int *pmonth, int *pday);
 
-char *month_name(int n) {
-
-   static char *name[] = {
-       "Illegal month",
-       "January", "February", "March",
-       "April", "May", "June",
-       "July", "August", "September",
-       "October", "November", "December"
-   };
-
-   return (n < 1 || n > 12) ? name[0] : name[n];
+char *month_name(int n)
+{
+    static char *name[] = {
+        "Illegal month",
+        "January", "February", "March",
+        "April", "May", "June",
+        "July", "August", "September",
+        "October", "November", "December"};
+    return (n < 1 || n > 12) ? name[0] : name[n];
 }
 
-void month_day(int year, int yearday, int *pmonth, int *pday) {
-    int leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+void month_day(int year, int yearday, int *pmonth, int *pday)
+{
+    int leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     *pday = yearday;
 
-    do {
-        for (int i = 1; i < 13; i++) {
-            *pmonth = i;
-
-            if (*pday <= 31) {           
-                return;
-            }
-            *pday -= daytab[leap][i - 1];
+    for (int i = 1; i < 13; i++)
+    {
+        *pmonth = i;
+        if (*pday <= 31)
+        {
+            return;
         }
-        year++;
-        leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
-        plus++;
-    } while(*pday > 31);
+        *pday -= daytab[leap][i - 1];
+    }
 }
 
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     int year = atoi(argv[1]),
         yearday = atoi(argv[2]),
         pmonth = 0,
         pday = 0;
 
+    if (yearday > 366)
+    {
+        printf("Invalid yearday\n");
+        return 0;
+    }
+
     month_day(year, yearday, &pmonth, &pday);
-    printf("%s %d, %d\n", month_name(pmonth), pday, year + plus);
+    printf("%s %d, %d\n", month_name(pmonth), pday, year);
     return 0;
 }
