aab0af8d1dc653cbad5fb5183797f90336c20ab8
Rey Vega <A01114523@itesm.mx>
2021-03-17 22:40:16 -0700

    Solved Lab - String Manipulation Library
    Signed-off-by: Rey Vega <A01114523@itesm.mx>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+extern int mystrlen(char *);
+char *mystradd(char *, char *);
+int mystrfind(char *, char *);
+
+int main(int argc, char *argv[]) {
+    
+    if (argc == 4 && strcmp(argv[1], "-add") == 0) {
+        printf("Initial Lenght      : %d\n", mystrlen(argv[2]));
+        char *new_string = mystradd(argv[2],argv[3]);
+        printf("New String          : %s\n", new_string);
+        printf("New length          : %d\n", mystrlen(new_string));
+        return 0;
+    }
+
+    if (argc == 4 && strcmp(argv[1], "-find") == 0) {
+        int find = mystrfind(argv[2], argv[3]);
+        if (find > -1) {
+            printf("['%s'] string was found at [%d] position\n", argv[3], find);
+        } else {
+            printf("No ocurrences found");
+        }
+        return 0;
+    }
+    printf("Error: missing arguments or bad inputs\n");
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,47 @@
 
 
-int mystrlen(char *str){
-    return 0;
+int mystrlen(char *str)
+{
+    int len = 0;
+    while (*str != '\0')
+    {
+        str++;
+        len++;
+    }
+    return len;
 }
 
-char *mystradd(char *origin, char *addition){
-    return 0;
+char *mystradd(char *origin, char *addition)
+{
+    char *result = origin + mystrlen(origin);
+
+    while (*addition != '\0')
+    {
+        *result++ = *addition++;
+    }
+
+    *result = '\0';
+    return origin;
 }
 
-int mystrfind(char *origin, char *substr){
-    return 0;
+int mystrfind(char *origin, char *substr)
+{
+    int len = 0,
+        len2 = 0,
+        verification = mystrlen(substr),
+        tmp = 0;
+
+    while (origin[len] != '\0') {
+        tmp = len;
+        while (origin[len] == substr[len2] && origin[len] != '\0') {
+            len++;
+            len2++;
+        }
+        if (len2 == verification) {
+            return tmp;
+        }
+        len++;
+        len2 = 0;
+    }
+    return -1;
 }
